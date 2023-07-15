07db3f621e875276068e9879ea2a8e2b375e9de3
CCL Vietnamese <web@cclvietnamese.com.au>
2023-07-16 00:07:09 +1000

    test new form

--- src/pages/WebCCLVietnamesePages/WebCCLVietnameseEnquiry.js
+++ src/pages/WebCCLVietnamesePages/WebCCLVietnameseEnquiry.js
@@ -57,8 +57,6 @@ const ErrorMessage = tw.p`text-red-500 text-lg font-semibold mt-4`;
 
 const API_ENDPOINT = "https://cclvietnamese-proxy-server.azurewebsites.net/api/cclvietnamese-enquiry-form-test";
 
-
-
 export default ({
   subheading = "Contact Us",
   heading = <>Tư vấn khóa học </>,
@@ -75,57 +73,17 @@ export default ({
   formMethod = "get",
   textOnLeft = true,
 }) => {
-  // The textOnLeft boolean prop can be used to display either the text on left or right side of the image.
-  const [name, setName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
+  const [firstName, setFirstName] = useState("");
   const [emailAddress, setEmailAddress] = useState("");
-  const [facebookURL, setFacebookURL] = useState("");
-  const [promoCode, setPromoCode] = useState("");
-  const [engScore, setEngScore] = useState("");
-  const [boolCheckExam, setBoolCheckExam] = useState(false);
-  const [examDate, setExamDate] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     const data = {
-      answers: JSON.stringify([
-        {
-          questionId: "rd4312ca19ce845c2b511c7841ecf9a4a",
-          answer1: name,
-        },
-        {
-          questionId: "rcf2ad9c1c4594a13bdded5c45f0151d7",
-          answer1: phoneNumber,
-        },
-        {
-          questionId: "rfcf33b314514402f930c5b0ebe5ac6a8",
-          answer1: emailAddress,
-        },
-        {
-          questionId: "rf3c811aebf2f4ac19234d3a13e21dff7",
-          answer1: facebookURL,
-        },
-        {
-          questionId: "r7181eb5c216042558af17449c12cfafc",
-          answer1: boolCheckExam,
-        },
-        {
-          questionId: "r01241a0a68024a28a290f1841ad69bd3",
-          answer1: examDate,
-        },
-        {
-          questionId: "r02756120ead143f598a9d308a5f72f53",
-          answer1: engScore,
-        },
-        {
-          questionId: "r5fcfa4c2d6394491a412ab3e747006ea",
-          answer1: promoCode,
-        },
-      ]),
+      firstName: firstName,
+      emailAddress: emailAddress
     };
     try {
       const response = await fetch(API_ENDPOINT, {
@@ -140,29 +98,22 @@ export default ({
         throw new Error("An error occurred while submitting the form.");
       }
   
-      // Handle successful form submission (e.g., show a success message or redirect to another page)
       // Show a success message
       setSuccessMessage("Form submitted successfully!");
       setErrorMessage(""); // Clear any previous error message
 
       // Clear the input fields after successful form submission
-      setName("");
-      setPhoneNumber("");
+      setFirstName("");
       setEmailAddress("");
-      setFacebookURL("");
-      setPromoCode("");
-      setEngScore("");
-      setBoolCheckExam(false);
-      setExamDate("");
     } catch (error) {
       // Show an error message
       setErrorMessage("An error occurred while submitting the form.");
       setSuccessMessage(""); // Clear any previous success message
     }
   };
-    // ... The handleSubmit function implementation ...
-    return (
-      <PrimaryBackgroundContainer>
+
+  return (
+    <PrimaryBackgroundContainer>
         <FormContainer>
           <Content2Xl>
             <NavigationBar />
@@ -185,20 +136,8 @@ export default ({
                         type="text"
                         name="name"
                         placeholder="E.g. John Doe"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                      />
-                    </InputContainer>
-                    <InputContainer>
-                      <Label htmlFor="phone-input">Phone Number</Label>
-                      <Input
-                        id="phone-input"
-                        type="tel"
-                        name="phone"
-                        placeholder="E.g. +614-567-890"
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNumber(e.target.value)}
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
                         required
                       />
                     </InputContainer>
@@ -214,119 +153,6 @@ export default ({
                         required
                       />
                     </InputContainer>
-                    <InputContainer>
-                      <Label htmlFor="facebook-input">Facebook URL</Label>
-                      <Input
-                        id="facebook-input"
-                        type="url"
-                        name="facebook"
-                        placeholder="E.g. https://www.facebook.com/myprofile"
-                        value={facebookURL}
-                        onChange={(e) => setFacebookURL(e.target.value)}
-                        required
-                      />
-                    </InputContainer>
-                    <InputContainer>
-                      <Label htmlFor="exam-checked-input">Have you checked out the exam date</Label>
-                      <Select
-                        id="exam-checked-input"
-                        name="examChecked"
-                        value={boolCheckExam}
-                        onChange={(e) => setBoolCheckExam(e.target.value)}
-                        required
-                      >
-                        <option value="">Select an option</option>
-                        <option value="yes">Yes</option>
-                        <option value="no">No</option>
-                      </Select>
-                    </InputContainer>
-                    {boolCheckExam === "yes" && (
-                      <InputContainer>
-                        <Label htmlFor="exam-day-input">What is your exam day?</Label>
-                        <Input
-                          id="exam-day-input"
-                          type="text"
-                          name="examDay"
-                          placeholder="Enter the exam day"
-                          value={examDate}
-                          onChange={(e) => setExamDate(e.target.value)}
-                          required
-                        />
-                      </InputContainer>
-                    )}
-                    {boolCheckExam === "no" && (
-                      <>
-                        <Description>
-                          Please have a look at the available CCL Test Dates:{" "}
-                          <a
-                            href="https://www.naati.com.au/test-date/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            https://www.naati.com.au/test-date/
-                          </a>
-                        </Description>
-                        <div>
-                          <input type="checkbox" id="step-1" name="step-1" />
-                          <label htmlFor="step-1">
-                            Choose "Credentialed Community Language Test" in Select Test Type
-                          </label>
-                        </div>
-                        <div>
-                          <input type="checkbox" id="step-2" name="step-2" />
-                          <label htmlFor="step-2">Choose "Vietnamese" in Select Language</label>
-                        </div>
-                        <div>
-                          <input type="checkbox" id="step-3" name="step-3" />
-                          <label htmlFor="step-3">Make your decision</label>
-                        </div>
-                        <InputContainer>
-                          <Label htmlFor="exam-day-input">What is your exam day?</Label>
-                          <Input
-                            id="exam-day-input"
-                            type="text"
-                            name="examDay"
-                            placeholder="Enter the exam day"
-                            value={examDate}
-                            onChange={(e) => setExamDate(e.target.value)}
-                            required
-                          />
-                        </InputContainer>
-                      </>
-                    )}
-                    <InputContainer>
-                      <Label htmlFor="promocode-input">
-                        Promo code (If applicable)
-                      </Label>
-                      <Input
-                        id="promocode-input"
-                        type="text"
-                        name="promo"
-                        placeholder="E.g. Optional"
-                        value={promoCode}
-                        onChange={(e) => setPromoCode(e.target.value)}
-                      />
-                    </InputContainer>
-                    <InputContainer>
-                      <Label htmlFor="pte-input">PTE Score</Label>
-                      <Select
-                        id="pte-input"
-                        name="englishscore"
-                        placeholder="Your English score"
-                        value={engScore}
-                        onChange={(e) => setEngScore(e.target.value)}
-                        required
-                      >
-                        <option value="ptehigh">PTE 79 or IELTS 8.0</option>
-                        <option value="ptenorm">PTE 65 or IELTS 7.0</option>
-                        <option value="pteless">
-                          Less than PTE 65 or IELTS 7.0
-                        </option>
-                        <option value="pteunknown">
-                          Never take such exam before
-                        </option>
-                      </Select>
-                    </InputContainer>
                     <SubmitButton type="submit" value="Submit">
                       Submit
                     </SubmitButton>
@@ -339,6 +165,6 @@ export default ({
           </TwoColumn>
         </FormContainer>
       </PrimaryBackgroundContainer>
-    );
+  );
 };
 
